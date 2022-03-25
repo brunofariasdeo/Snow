378ac4502d5feba13694d202979358d52a782b46
brunofariasdeo <jfarias@daitan.com>
2022-03-25 12:31:39 -0300

    Create default button style

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,83 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './button.css';
+import './button.scss';
+import clsx from 'clsx';
 
-const APPEARANCE = {
-  default: "default",
-}
-
-/**
- * Primary UI component for user interaction
- */
 export const Button = ({
-  className,
-  color,
-  disabled,
-  disabledShadow,
-  endIcon,
-  size,
-  startIcon,
-  text,
+  label,
   variant
 }) => {
-  let defaultValue;
-  let hover = false; 
-
-  if(className && className.includes("hover")) {
-    hover = true;
-  }
-
-  if (color || disabledShadow || endIcon || startIcon || size || variant) {
-    defaultValue = "";
-  }
-  else {
-    defaultValue = "button-default";
-  }
-
-  const textValue = text || "Default";
-  const variantValue = variant || "";
-
   return (
     <button 
-      // className={clsx(defaultValue, hover, variantValue, disabledShadow, startIcon || endIcon, size, color)}
-      disabled={disabled}
+      className={clsx(
+        variant
+      )}
     >
-      {startIcon}
-      {/* && <AiOutlineShoppingCart /> */}
-      
-      {textValue}
-      {endIcon} 
-      {/* && <AiOutlineShoppingCart />} */}
+      {label}
     </button>
   )
 }
 
 Button.propTypes = {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary: PropTypes.bool,
-  /**
-   * What background color to use
-   */
-  backgroundColor: PropTypes.string,
-  /**
-   * How large should the button be?
-   */
-  size: PropTypes.oneOf(['small', 'medium', 'large']),
-  /**
-   * Button contents
-   */
-  label: PropTypes.string.isRequired,
-  /**
-   * Optional click handler
-   */
-  onClick: PropTypes.func,
+  variant: PropTypes.oneOf([
+    "default",
+  ]),
+
+  label: PropTypes.string,
 };
 
 Button.defaultProps = {
-  backgroundColor: null,
-  primary: false,
-  size: 'medium',
-  onClick: undefined,
+  variant: "default",
+  label: "Default"
 };
